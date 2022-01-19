67b6ecc2a28ceece1e2a4497fa0b01aa450304e5
Dnyanesh Walwadkar <44721153+dnyanshwalwadkar@users.noreply.github.com>
2022-01-19 22:24:26 +0000

    Update DataTransformation.py

--- DataTransform_Training/DataTransformation.py
+++ DataTransform_Training/DataTransformation.py
@@ -9,9 +9,7 @@ class dataTransform:
      """
                This class shall be used for transforming the Good Raw Training Data before loading it in Database!!.
 
-               Written By: iNeuron Intelligence
-               Version: 1.0
-               Revisions: None
+               
 
                """
 
